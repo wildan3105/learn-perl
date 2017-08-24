2c874e05bd266d9ea586ba1792221c9400b6777c
wldn <wildansnahar@gmail.com>
2017-08-24 23:01:52 +0700

    hashes exercise

---
+++ hashes-exercise.pl
@@ -0,0 +1,8 @@
+@family = ('Homer', 'Marge', 'Maggie');
+%shoe_color = ('Lisa' => 'red', 'Homer' => 'brown', 'Maggie' => 'pink', 'Marge' => 'blue', 'Bart' => 'yellow');
+%shoe_size = ('Marge' => 10, 'Lisa' => 7, 'Homer' => 12, 'Bart' => 8, 'Maggie' => 4);
+# start your code here
+
+print "$family[0] wears $shoe_color{$family[0]} shoes size $shoe_size{$family[0]} \n";
+print "$family[1] wears $shoe_color{$family[1]} shoes size $shoe_size{$family[1]} \n";
+print "$family[2] wears $shoe_color{$family[2]} shoes size $shoe_size{$family[2]} \n";
