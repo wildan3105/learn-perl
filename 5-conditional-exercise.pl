a8b7c62f742a9067528fca381a29e1999a1e0399
wldn <wildansnahar@gmail.com>
2017-08-25 22:41:17 +0700

    exercise with no resolve

---
+++ 5-conditional-exercise.pl
@@ -0,0 +1,5 @@
+@family = ('Homer', 'Moe', 'Maggie');
+%shoe_color = ('Lisa' => 'red', 'Homer' => 'brown', 'Maggie' => 'pink', 'Marge' => 'blue', 'Bart' => 'yellow');
+%shoe_size = ('Moe' => 9, 'Lisa' => 7, 'Homer' => 12, 'Bart' => 8, 'Maggie' => 4);
+
+# expected sample output : Homer wears large brown shoes size 12
