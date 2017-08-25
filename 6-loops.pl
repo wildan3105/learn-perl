b114f73967102f2797d167607d4bb391e1c21347
wldn <wildansnahar@gmail.com>
2017-08-25 22:46:27 +0700

    loops

---
+++ 6-loops.pl
@@ -0,0 +1,18 @@
+# while 
+$count = 10;
+while($count > 0){
+    print "Increment to : $count \n";
+    $count--;
+}
+
+# until
+$hitung = 1;
+until ($hitung > 10){
+    print "Berhitung ke: $hitung \n";
+    $hitung++;
+}
+
+# for
+for($i =1; $i<10; $i++){
+    print "Count i to : $i \n";
+}
